f185648836246b02d59c9efb4e253fba36b26bdc
pastoriana <anabeatrizpastori@gmail.com>
2024-07-13 15:58:20 -0300

    atualizacao-run

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,6 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import 'zone.js';
+// src/main.ts
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
-
-bootstrapApplication(App);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
